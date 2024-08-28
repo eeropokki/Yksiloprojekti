38a79f843400f1902faf06bb5f64328aadd9f10f
Eero Pokki <s2200880@edu.bc.fi>
2024-08-28 12:57:21 +0300

    Taso1
    SuperTiled2Unity ja aloitin 1. tason tekemisen

---
+++ Yksilöprojekti/Assets/Tiled/Yksilöprojekti/Background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Background" tilewidth="16" tileheight="16" tilecount="160" columns="8">
+ <image source="../../Sprites/fourSeasonsPlatformer_ [tileset, version 2.0]/background_/terrain_.png" width="128" height="320"/>
+</tileset>
