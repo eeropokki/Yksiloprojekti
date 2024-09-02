5da5589798ef39c3a316a42f73a213761e1f36b5
Eero Pokki <s2200880@edu.bc.fi>
2024-09-02 14:56:43 +0300

    Pelaajan liikkeet ja 1. vihollisen liikkeet

--- Yksilöprojekti/Assets/Scripts/PlayerController.cs
+++ Yksilöprojekti/Assets/Scripts/PlayerController.cs
@@ -1,28 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
 
-    //Globaali muuttuja
-    public static PlayerController instance;
+    public KeyCode left;
+    public KeyCode right;
+    public KeyCode jump;
 
-    [Header("LIIKKUMINEN")]
-    public bool canMove;
-    public float moveSpeed = 4f;
-    private Rigidbody rb;
-    private Vector2 move;
+    private Rigidbody2D rb2d;
+
+    private SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MovePlayer();
+
     }
+
+    private void MovePlayer()
+    {
+        if (Input.GetKeyDown(left))
+        {
+            rb2d.velocity = new Vector2(-moveSpeed, rb2d.velocity.y);
+
+            spriteRenderer.flipX = true;
+        }
+        else if (Input.GetKeyDown(right))
+        {
+            rb2d.velocity = new Vector2(moveSpeed, rb2d.velocity.y);
+
+            spriteRenderer.flipX = false;
+        }
+        else
+        {
+            if (rb2d.velocity.y != 0)
+            {
+                rb2d.velocity = new Vector2(rb2d.velocity.x, rb2d.velocity.y);
+            }
+            else
+            {
+                rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+            }
+        }
+
+        if (Input.GetKeyDown(jump))
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
+        }
+    }
+
 }
---
+++ Yksilöprojekti/Assets/Tiled/Yksilöprojekti/eventBlock_.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="eventBlock_" tilewidth="16" tileheight="16" tilecount="84" columns="14">
+ <image source="eventBlock_.png" width="224" height="96"/>
+</tileset>
