ef3aba49994e3ea8f5389e67b59e7851fc4ac11a
Eero Pokki <s2200880@edu.bc.fi>
2024-09-12 14:38:58 +0300

    Paranneltu playerController

--- Yksilöprojekti/Assets/Scripts/Player/PlayerController.cs
+++ Yksilöprojekti/Assets/Scripts/Player/PlayerController.cs
@@ -1,78 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.PlayerLoop;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed;
-    [SerializeField] private float jumpForce;
 
-    public KeyCode left;
-    public KeyCode right;
-    public KeyCode jump;
+    PlayerControls controls;
+    
+    
+    float direction = 0;
 
-    private Rigidbody2D rb2d;
+    public Rigidbody2D playerRb;
 
-    private SpriteRenderer spriteRenderer;
-
-    AudioManager audioManager;
+    public float speed = 400;
+    public float jumpForce = 5;
+    bool isGrounded;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
 
     private void Awake()
     {
-        //audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
-    }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb2d = GetComponent<Rigidbody2D>();
+        
+        controls = new PlayerControls();
+        controls.Enable();
 
-        spriteRenderer = GetComponent<SpriteRenderer>();
-    }
+        controls.Land.Move.performed += context =>
+        {
+            direction = context.ReadValue<float>();
+        };
+
+        controls.Land.Jump.performed += context => Jump();
+        
+    } 
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        MovePlayer();
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+        Debug.Log(isGrounded);
+        playerRb.velocity = new Vector2(direction * speed * Time.fixedDeltaTime, playerRb.velocity.y);
         
     }
 
-    private void MovePlayer()
+    private void Jump()
     {
-        if (Input.GetKeyDown(left))
-        {
-            rb2d.velocity = new Vector2(-moveSpeed, rb2d.velocity.y);
-
-            //audioManager.PlaySFX(audioManager.Walking);
-
-            spriteRenderer.flipX = true;
-        }
-        else if (Input.GetKeyDown(right))
-        {
-            rb2d.velocity = new Vector2(moveSpeed, rb2d.velocity.y);
-
-            //audioManager.PlaySFX(audioManager.Walking);
-
-            spriteRenderer.flipX = false;
-        }
-        else
-        {
-            if (rb2d.velocity.y != 0)
-            {
-                rb2d.velocity = new Vector2(rb2d.velocity.x, rb2d.velocity.y);
-            }
-            else
-            {
-                rb2d.velocity = new Vector2(0, rb2d.velocity.y);
-            }
-        }
-
-        if (Input.GetKeyDown(jump))
-        {
-            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
-        }
+        Debug.Log("Player Jump");
+        playerRb.velocity = new Vector2(playerRb.velocity.x, jumpForce);
     }
-
 }
