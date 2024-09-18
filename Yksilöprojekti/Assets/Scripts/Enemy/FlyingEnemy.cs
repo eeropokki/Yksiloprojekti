fd6cd2877e82c6f9771cdb32d9f7cc86f51a9d7e
Eero Pokki <s2200880@edu.bc.fi>
2024-09-18 11:52:19 +0300

    2. Vihollinen
    2. vihollisen koodi

---
+++ Yksilöprojekti/Assets/Scripts/Enemy/FlyingEnemy.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingEnemy : MonoBehaviour
+{
+    public float speed;
+    public bool chase = false;
+    public Transform startingPoint;
+    private GameObject player;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player == null)
+        {
+            return;
+        }
+        if (chase == true)
+        {
+            Chase();
+        }
+        else
+        {
+            // go to staring position
+            ReturnStartPoint();
+        }
+       
+        Flip();
+    }
+    void Chase()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, 
+            player.transform.position, speed * Time.deltaTime);
+    }
+    
+    void ReturnStartPoint()
+    {
+        transform.position = Vector2.MoveTowards(transform.position,
+            startingPoint.position, speed * Time.deltaTime);
+    }
+
+    void Flip()
+    {
+        if (transform.position.x > player.transform.position.x)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+    }
+}
